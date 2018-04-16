cbd5ecbc7d341f29695789cefe6b552c62cc7463
Rafa La Buonora <rafa@Rafas-MacBook-Pro.local>
2018-04-15 22:03:55 -0300

    Add offense/defense

--- week1/salaries.R
+++ week1/salaries.R
@@ -2,19 +2,37 @@ library(tidyverse)
 library(readxl)
 library(dplyr)
 library(ggplot2)
+library(stringr)
+library(ggthemes)
 
 df <- read_xlsx('./tidytuesday/data/tidy_tuesday_week2.xlsx') %>% 
-  as_tibble
-
-# Data Frame original tiene 100 lineas por anio
-# (podrian ser 100 cuadros, 100 jugadores?) supongo que son jujadores
-
-df %>% 
-  gather(-year, key="position", val="cap") %>% 
-  ggplot(aes(year, cap)) + 
-  geom_jitter() +
-  geom_smooth() +
-  facet_wrap(~position)
+  as_tibble  %>% 
+  gather(-year, key="position", val="salary") %>% 
+  group_by(year, position) %>% 
+  top_n(n = 16, wt = salary) %>% 
+  mutate(salary = salary/1000000,
+         year_label = (str_sub(year, 3, 4)),
+         of_def = case_when(
+           position %in% c("Cornerback", "Defensive Lineman",
+                           "Safety", "Linebacker",
+                           "Special Teamer") ~ "Defense",
+           position %in% c("Running Back", "Quarterback",
+                           "Offensive Lineman", "Wide Receiver",
+                           "Tight End") ~ "Offense"
+         )
+  )
+         
+df  %>% 
+  ggplot(aes(year, salary)) + 
+  geom_jitter(aes(alpha=0.5, col=position)) +
+  scale_y_continuous(name="Average Cap") +
+  scale_x_continuous(labels=c("'12", "'14", "'16", "'18"),
+                     breaks = c(2012, 2014, 2016, 2018)) + 
+  labs(title = "The average pay for top running backs has stalled", 
+       subtitle = "Average cap value of the 16 highest-paid players in each position") +
+  geom_smooth(col = "orange") +
+  facet_wrap(facets= of_def~position) +
+  theme(legend.position = "none")
 
 
 ggsave("week1/plot.png")  

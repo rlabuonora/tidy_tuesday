14365a2f323c2f5db8690a5569cda4e1dbd06643
Rafa La Buonora <rafa@Rafas-MacBook-Pro.local>
2018-04-15 22:43:36 -0300

    Add second plot

--- week1/salaries.R
+++ week1/salaries.R
@@ -5,13 +5,15 @@ library(ggplot2)
 library(stringr)
 library(ggthemes)
 
+
+# import and tidy data
 df <- read_xlsx('./tidytuesday/data/tidy_tuesday_week2.xlsx') %>% 
   as_tibble  %>% 
   gather(-year, key="position", val="salary") %>% 
   group_by(year, position) %>% 
   top_n(n = 16, wt = salary) %>% 
+  ungroup %>% 
   mutate(salary = salary/1000000,
-         year_label = (str_sub(year, 3, 4)),
          of_def = case_when(
            position %in% c("Cornerback", "Defensive Lineman",
                            "Safety", "Linebacker",
@@ -21,7 +23,16 @@ df <- read_xlsx('./tidytuesday/data/tidy_tuesday_week2.xlsx') %>%
                            "Tight End") ~ "Offense"
          )
   )
-         
+
+df_percent <- df %>% 
+  group_by(year) %>% 
+  mutate(total_year_salary = sum(salary)) %>% 
+  group_by(position, add = TRUE) %>% 
+  summarise(salary_participation = sum(salary) * 100 / first(total_year_salary),
+            of_def = first(of_def))
+
+
+# plot   
 df  %>% 
   ggplot(aes(year, salary)) + 
   geom_jitter(aes(alpha=0.5, col=position)) +
@@ -32,7 +43,13 @@ df  %>%
        subtitle = "Average cap value of the 16 highest-paid players in each position") +
   geom_smooth(col = "orange") +
   facet_wrap(facets= of_def~position) +
-  theme(legend.position = "none")
+  theme(legend.position = "none") +
+  ggsave("week1/plot2.png", width=6, height=6)
 
 
-ggsave("week1/plot.png")  
+
+df_percent %>% 
+  ggplot(aes(year, salary_participation)) +
+  geom_line(aes(group=position, col=position)) +
+  facet_wrap(~of_def) + 
+  ggsave("week1/plot1.png")  
